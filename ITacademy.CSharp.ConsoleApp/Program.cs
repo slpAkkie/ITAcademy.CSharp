c2982a3926d8f05f4226831ed4ef37d41ca197b6
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-06-27 13:58:27 +0300

    Добавьте файлы проекта.

---
+++ ITacademy.CSharp.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITacademy.CSharp.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("\nPress any key to close...");
+            Console.ReadKey();
+        }
+    }
+}
