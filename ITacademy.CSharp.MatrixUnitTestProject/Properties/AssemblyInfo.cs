b7914122a6da0a30c13acc4c6c5712ad64cc458e
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-06-28 16:37:58 +0300

    Seventh task

---
+++ ITacademy.CSharp.MatrixUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ITacademy.CSharp.MatrixUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ITacademy.CSharp.MatrixUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6495e971-784f-4ad8-8ecd-82854c0f9b1f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
