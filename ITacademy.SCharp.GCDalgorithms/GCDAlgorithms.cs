88a360bf4465bedbaf71ee3777d2e2f0f47d1167
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-06-28 13:31:47 +0300

    Fourth task

---
+++ ITacademy.SCharp.GCDalgorithms/GCDAlgorithms.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace ITacademy.CSharp.GCDalgorithms
+{
+    static class GCDAlgorithms
+    {
+        /// <summary>
+        /// Find the GCD for 2 numbers with Euclid method
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int FindGCDEuclid(int a, int b)
+        {
+            if (a == 0) return b;
+
+            while (b > 0)
+            {
+                if (a > b) a -= b;
+                else b -= a;
+            }
+
+            return a;
+        }
+    }
+}
