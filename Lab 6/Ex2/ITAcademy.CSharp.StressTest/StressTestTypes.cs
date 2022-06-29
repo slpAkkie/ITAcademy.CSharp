8c7b14200dc018b6334d77cec82795255c79f260
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-06-29 20:04:09 +0300

    Lab 6: Ex2

---
+++ Lab 6/Ex2/ITAcademy.CSharp.StressTest/StressTestTypes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ITAcademy.CSharp.StressTest
+{
+    // Enumerations Exercise 1
+
+    /// <summary>
+    /// Enumeration of girder material types
+    /// </summary>
+    public enum Material { StainlessSteel, Aluminium, ReinforcedConcrete, Composite, Titanium }
+
+    /// <summary>
+    /// Enumeration of girder cross-sections
+    /// </summary>
+    public enum CrossSection { IBeam, Box, ZShaped, CShaped }
+
+    /// <summary>
+    /// Enumeration of test results
+    /// </summary>
+    public enum TestResult { Pass, Fail }
+
+    public struct TestCaseResult
+    {
+        public TestResult Result;
+        public string ReasonForFailure;
+    }
+}
---
+++ Lab 6/Ex2/ITAcademy.CSharp.Structues/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ITAcademy.CSharp.StressTest;
+
+namespace ITAcademy.CSharp.Structues
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // TODO - Declare a TestCaseResult array
+        TestCaseResult[] testCaseResults;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Create some sample Test Case Results and display
+        /// in a ListBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>       
+        private void RunTests_Click(object sender, RoutedEventArgs e)
+        {
+            reasonsList.Items.Clear();
+            testCaseResults = new TestCaseResult[10];
+
+            for (int i = 0; i < testCaseResults.Length; i++)
+                testCaseResults[i] = TestManager.GenerateResult();
+
+            int passCount = 0, failCount = 0;
+
+            for (int i = 0; i < testCaseResults.Length; i++)
+                if (testCaseResults[i].Result == TestResult.Pass)
+                    passCount++;
+                else
+                {
+                    failCount++;
+                    reasonsList.Items.Add(testCaseResults[i].ReasonForFailure);
+                }
+
+            successLabel.Content = $"Successes: {passCount}";
+            failureLabel.Content = $"Failures: {failCount}";
+        }
+    }
+}
