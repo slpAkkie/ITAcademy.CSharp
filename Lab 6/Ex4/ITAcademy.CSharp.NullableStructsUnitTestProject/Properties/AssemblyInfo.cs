202b0913b238eed2a0e79069fff851da5cd83d0a
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-06-29 20:56:34 +0300

    Lab 6: Ex4

---
+++ Lab 6/Ex4/ITAcademy.CSharp.NullableStructsUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ITAcademy.CSharp.NullableStructsUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ITAcademy.CSharp.NullableStructsUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("17a3c350-c5a2-4115-a5c5-37c501b77245")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lab 6/Ex4/ITAcademy.CSharp.StressTest/Utility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ITAcademy.CSharp.StressTest
+{
+    static class Utility
+    {
+        public static Random rand = new Random();
+    }
+}
