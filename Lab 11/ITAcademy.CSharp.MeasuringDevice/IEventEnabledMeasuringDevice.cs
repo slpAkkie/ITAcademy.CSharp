c2ecf2b80cc72b3507723f0b4508fdeb9cba3e08
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-07-02 10:31:10 +0300

    Lab 11

---
+++ Lab 11/ITAcademy.CSharp.MeasuringDevice/IEventEnabledMeasuringDevice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITAcademy.CSharp.MeasuringDevice
+{
+    internal interface IEventEnabledMeasuringDevice : IMeasuringDevice
+    {
+        event EventHandler NewMeasurementTaken;
+    }
+}
