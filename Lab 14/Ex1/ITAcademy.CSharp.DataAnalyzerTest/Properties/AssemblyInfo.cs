f347684a29dc3ece7b6963c6fe7a056b54a8df2b
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-07-02 12:29:26 +0300

    Lab 14
    It's not working just cause I have another names and file structure than the exersice expected

---
+++ Lab 14/Ex1/ITAcademy.CSharp.DataAnalyzerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ITAcademy.CSharp.StressTestAnalyzerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ITAcademy.CSharp.StressTestAnalyzerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("518424ea-44b6-4281-911d-68fa0636b054")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
