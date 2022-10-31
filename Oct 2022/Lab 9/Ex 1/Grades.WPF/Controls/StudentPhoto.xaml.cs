7a6535fc40c4c85139d5c67a9499f2bfb7d4ec46
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-10-31 16:47:34 +0300

    Lab 9 Ex 1, 2

---
+++ Oct 2022/Lab 9/Ex 1/Grades.WPF/Controls/StudentPhoto.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Grades.WPF
+{
+    public partial class StudentPhoto : UserControl
+    {
+        public StudentPhoto()
+        {
+            InitializeComponent();
+        }
+    }
+}
