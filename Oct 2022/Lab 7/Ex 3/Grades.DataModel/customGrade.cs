94d8a26120945191580b34e3df4b08157a996f28
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-10-31 16:45:45 +0300

    Lab 7 Ex 1, 2, 3

---
+++ Oct 2022/Lab 7/Ex 3/Grades.DataModel/customGrade.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Grades.DataModel
+{
+    public partial class Grade
+    {
+        public bool ValidateAssessmentDate(DateTime assessmentDate)
+        {
+            if (assessmentDate > DateTime.Now)
+                throw new ArgumentOutOfRangeException("Assessment Date", "Assessment date must be on or before the current date");
+
+            return true;
+        }
+
+        public bool ValidateAssessmentGrade(string assessment)
+        {
+            Match matchGrade = Regex.Match(assessment, @"^[A-E][+-]?$");
+            if (!matchGrade.Success)
+                throw new ArgumentOutOfRangeException("Assessment", "Assessment grade must be in the range A+ to E-");
+
+            return true;
+        }
+    }
+}
---
+++ Oct 2022/Lab 7/Ex 3/Grades.DataModel/customTeacher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.DataModel
+{
+    public partial class Teacher
+    {
+        private const int MAX_CLASS_SIZE = 8;
+
+        public void EnrollInClass(Student student)
+        {
+            int numStudents = (from s in Students
+                               where s.TeacherUserId == UserId
+                               select s).Count();
+
+            if (numStudents >= MAX_CLASS_SIZE)
+                throw new ClassFullException("Class full: Unable to enroll student", Class);
+
+            if (student.TeacherUserId == null)
+                student.TeacherUserId = UserId;
+            else
+                throw new ArgumentException("Student", "Student is already assigned to a class");
+        }
+    }
+}
